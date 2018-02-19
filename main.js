a19c10f53d894e9a40aa9b20602f661f862b4ce3
Chanduniverse <chand4ara@gmail.com>
2018-02-19 16:42:53 -0500

    implement json database

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
-xlsxj = require("xlsx-to-json");
- xlsxj({
-   input: "RDB.xlsx",
-   output: "rdb.json"
- }, function(err, result) {
-   if(err) {
-     console.error(err);
-   }else {
-     console.log(result);
-   }
- });
+let d = new Date();
+        alert("Today's date is " + d);
